af5528d10bb6eb5d77a5107f2dca47c0b5c22e73
danil <meshcheryakov@sima-land.ru>
2018-11-05 00:21:33 +0500

    remove parallel sieve

--- any/drone-target/src/app/coordinates/transformer.go
+++ any/drone-target/src/app/coordinates/transformer.go
@@ -2,74 +2,9 @@ package coordinates
 
 import (
 	"math"
-	"sync"
 )
 
-func ParallelSieve(pos int) (ulamPos int) {
-	ulamPos = 0
-	if pos > 1 {
-		numsCnt := pos + 1
-		nums := make([]int, numsCnt)
-		nums[0] = 1
-		nums[1] = 0
-		nums[2] = 1
-		if pos > 2 {
-			nums[3] = 1
-		}
-		if pos > 4 {
-			nums[5] = 1
-		}
-		stepsDifs := [8]int{4, 2, 4, 2, 4, 6, 2, 6}
-		k := 7
-		for i := 0; k <= pos; i++ {
-			nums[k] = 1
-			k += stepsDifs[i % 8]
-		}
-		primeProgressionSteps := [8]int{7, 11, 13, 17, 19, 23, 29, 31}
-		notPrimes := make(chan int, 10)
-		var wg sync.WaitGroup
-		wg.Add(8)
-		for i := range primeProgressionSteps {
-			numsCp := make([]int, numsCnt)
-			copy(numsCp, nums)
-			step := primeProgressionSteps[i]
-			go func() {
-				defer wg.Done()
-				j := 0
-				g := 0
-				for k := step; int(math.Pow(float64(k), 2)) <= pos; k = 30 * j + step {
-					if nums[k] == 1 {
-						for l := int(math.Pow(float64(k), 2)); l <= pos; l += k {
-							notPrimes <- l
-							g++
-						}
-					}
-					j++
-				}
-				println(j + g)
-			}()
-		}
-		go func() {
-			for m := range notPrimes {
-				nums[m] = 0
-			}
-		}()
-		wg.Wait()
-		if nums[pos] == 1 {
-			ulamPos = -1
-		} else {
-			for i := range nums {
-				if nums[i] == 0 {
-					ulamPos += 1
-				}
-			}
-		}
-	} else {
-		ulamPos = pos
-	}
-	return ulamPos
-}
-
+// Sieve counts not prime numbers using Eratosthenes sieve
 func Sieve(pos int) (ulamPos int) {
 	ulamPos = 0
 	if pos > 1 {
@@ -101,7 +36,7 @@ func Sieve(pos int) (ulamPos int) {
 		} else {
 			for i := range nums {
 				if nums[i] == 0 {
-					ulamPos += 1
+					ulamPos++
 				}
 			}
 		}
@@ -111,6 +46,7 @@ func Sieve(pos int) (ulamPos int) {
 	return ulamPos
 }
 
+// UlamToCartesian transforms place on modified Ulam spiral to cartesian coordinates
 func UlamToCartesian(pos int) (x, y int) {
 	closestSqrt := int(math.Floor(math.Sqrt(float64(pos))))
 	minOddSqrt := closestSqrt
